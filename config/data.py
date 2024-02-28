d9481e05be04d8c3df3eb997b25f240241e77f0b
Nikita0806 <volkov_n@garpix.com>
2024-02-28 10:25:21 +0300

    good test3

--- config/data.py
+++ config/data.py
@@ -15,6 +15,3 @@ class Data:                             # тут храним данные в с
     R_PASSWORD = os.getenv("R_PASSWORD")
     R_REPEAT_PASSWORD = os.getenv("R_REPEAT_PASSWORD")
 
-    PHOTO_1 = os.getenv("PHOTO1")
-    PHOTO_2 = os.getenv("PHOTO2")
-    PHOTO_3 = os.getenv("PHOTO3")
